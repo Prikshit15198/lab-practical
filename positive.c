e37fa61e9897ae5f235e1d4d4164257b6cfa1f2d
Prikshit15198 <Prikshit5555@gmail.com>
2025-09-22 23:39:58 +0530

    code uploded

---
+++ positive.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+int num, positive = 0, negative = 0, zero = 0;
+char choice;
+
+for( ; ; ) {  
+printf("Enter a number: ");
+scanf("%d", &num);
+
+if (num > 0){ positive++;}
+else if (num < 0){ negative++;}
+else{ zero++;}                             
+
+printf("Do you want to enter the no. (y/n): ");
+scanf(" %c", &choice);
+if (choice == 'n'){
+break;}
+}
+printf("Positive no. : %d\n", positive);
+printf("negeative no,: %d\n", negative);
+printf("Zeros: %d\n", zero);
+
+return 0;
+}
